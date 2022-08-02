402ecf9ebc8485b7f57ef690cd161b9e446f3292
rifandiif <65202945+rifandiif@users.noreply.github.com>
2022-08-02 17:20:26 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -14,8 +14,8 @@ global.APIKeys = {
 
 // setting 
 global.autoread = false // auto read message
-global.autobio = true 
-global.userRegister = true 
+global.autobio = false 
+global.userRegister = false 
 
 //language
 //Available in indonesia , english & spanyol 
@@ -25,23 +25,23 @@ global.language = indonesia  //change indonesia to english if you don't understa
 
 // Other
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername= "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
-global.myweb2 = "https://linktr.ee/zeeoneofc" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
+global.botname = "RFSTOREPBGBOT" //namabot kalian
+global.ownername= "Rifandi" //nama kalian
+global.myweb ="https://instagram.com/rfstorepbg" //bebas asal jan hapus
+global.youtube = "https://youtube.com/c/RFProject" //bebas asal jan hapus
+global.myweb2 = "https://instagram.com/rifandiif" //bebas
+global.email = "akunrfstok2@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
 global.premium = ['62887435047326'] //premium user
-global.owner = ["622150996855","622150996855"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+62887435047326" //nmr wa kalian
+global.owner = ["6285726269397","6285172429397"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "6285726269397" // nomor wa kalian
+global.ownernomerr = "+6285726269397" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.packname = '© RFSTOREPBG' //sticker wm ubah
+global.author = 'Di Buat Oleh RFSTOREPBG' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 global.multiplier = 69
